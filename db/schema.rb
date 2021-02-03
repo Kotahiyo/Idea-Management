0891c43fbb8a045ec3d86c07e569717dd6cc63ed
Kouhei <hiyotihakou@gmail.com>
2021-02-03 11:22:07 +0900

    idea モデル作成。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_005552) do
+ActiveRecord::Schema.define(version: 2021_02_03_011920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,13 @@ ActiveRecord::Schema.define(version: 2021_02_03_005552) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "ideas", force: :cascade do |t|
+    t.text "body", null: false
+    t.bigint "categories_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["categories_id"], name: "index_ideas_on_categories_id"
+  end
+
+  add_foreign_key "ideas", "categories", column: "categories_id"
 end
