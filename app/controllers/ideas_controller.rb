b797fdc9d2d9ced24c2fce71cb27a03cc5aa5b56
Kouhei <hiyotihakou@gmail.com>
2021-02-05 02:58:31 +0900

    ステータスコードの指定

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,7 +1,16 @@
 class IdeasController < ApplicationController
   def index
-    ideas = Category.joins(:ideas).select('ideas.id, name AS category, body')
 
-    render json: { data: ideas }
+    if params[:idea][:category_name] == ""
+
+      ideas = Category.joins(:ideas).select('ideas.id, name AS category, body')
+
+    else
+
+      ideas = Category.joins(:ideas).where(name: params[:idea][:category_name]).select('ideas.id, name AS category, body')
+
+    end
+
+    render json: { data: ideas }, status: :not_found
   end
 end
